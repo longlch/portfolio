5f4e75b73191ed886d93956602802e2313f1cd66
hoanglonglch <hoanglonglch@gmail.com>
2020-03-08 15:46:55 +0700

    [Feature-003] Way2 Add FilterLink event

--- src/components/redux-example/components/App.js
+++ src/components/redux-example/components/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import AddTodo from "../containers/AddTodo";
 import VisibleTodoList from "../containers/VisibleTodoList";
 import FilterLinkContainer from "../containers/FilterLinkContainer";
+import Footer from "./Footer";
 
 const App = ()=> (
     <div>
         <AddTodo/>
         <VisibleTodoList/>
-        <FilterLinkContainer/>
+        <Footer/>
     </div>
 )
 
